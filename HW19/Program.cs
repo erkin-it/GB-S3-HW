8e2b5e7fcf3ac04a546a072cb0627199e9a9f8dc
Erkin IT <erkin.it@gmail.com>
2023-07-06 05:20:36 +0600

    Add home work 19

---
+++ HW19/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+bool CheckPalindrom(int CheckNum)
+{
+    int a = CheckNum;
+    int b = 0;
+    int c = 0;
+    do 
+        {
+            c=a%10;
+            b=b*10+c;
+            a=a/10;
+        }
+    while (a > 0);
+
+    if (CheckNum == b)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+
+//------------------------------------------------------------------------
+
+//Ввод числа
+Console.Write("Введите пятизначное положительное число: ");
+int NumToCheck = int.Parse(Console.ReadLine());
+
+//проверка
+
+if (NumToCheck > 9999 && NumToCheck < 100000)
+{
+    if (CheckPalindrom(NumToCheck))
+    {
+        Console.Write("Число является палиндромом");
+    } 
+    else
+    {
+        Console.Write("Число не является палиндромом");
+    }
+}
+else 
+{
+    Console.Write("Число не соответствует условию");
+}
